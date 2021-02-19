628e2090ce94f311ff30aa3ec068c256d5970048
Shaneve <shane.verhaeghe@student.odisee.be>
2021-02-19 01:45:21 +0100

    Oefeningen Les1

---
+++ Oefeningen/01.OOAD -herhaling en aanvulling/ConsoleBankautomaat/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace _01.OOAD_herhaling_en_aanvulling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int saldo = 500, storten, afhalen;
+            string keuze;
+            int pin = 0, x = 0;
+            Console.WriteLine("Bankautomaat\n");
+            Console.WriteLine("============\n\n");
+            Console.WriteLine("Gebruik pincode \n");
+            pin = int.Parse(Console.ReadLine());
+            while (true)
+            {
+                Console.WriteLine("\na. Afhaling \n");
+                Console.WriteLine("b. Storting \n");
+                Console.WriteLine("c. Stoppen \n");
+                Console.WriteLine("d. Uw huidig saldo \n");
+                Console.WriteLine("Je keuze: ");
+                keuze = Console.ReadLine();
+                switch (keuze)
+                {
+
+                    case "a":
+                        Console.WriteLine("\n Welk bedrag wil je afhalen: ");
+                        afhalen = int.Parse(Console.ReadLine());
+                        if (afhalen > saldo)
+                        {
+                            Console.WriteLine("\n Onvoldoende Saldo!");
+                        }
+                        else
+                        {
+                            saldo = saldo - afhalen;
+                            Console.WriteLine("\n\n Afhaling ok-");
+                            Console.WriteLine("het nieuw saldo is : {0}", saldo);
+                        }
+                        break;
+                    case "b":
+                        Console.WriteLine("\n Welk bedrag wil je storten: ");
+                        storten = int.Parse(Console.ReadLine());
+                        saldo = saldo + storten;
+                        Console.WriteLine("Storting ok-");
+                        Console.WriteLine("het nieuw saldo is : {0}", saldo);
+                        break;
+                    case "c":
+                        Console.WriteLine("\n Bedankt en tot ziens.");
+                        break;
+                    case "d":
+                        Console.WriteLine("\n Uw huidig saldo is : {0} ", saldo);
+                        break;
+                }
+            }
+        }
+    }
+}
+
+
---
+++ Oefeningen/01.OOAD -herhaling en aanvulling/WpfOxo/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace WpfOxo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool beurt = true; //true=X false=O
+        int beurtenTeller = 0; 
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_click(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender; //converteert de sender object
+            if (beurt)
+                btn.Content = "X";
+            else
+                btn.Content = "O";
+
+            beurt = !beurt; //om de beurt te bepalen
+            btn.IsEnabled = false;
+            beurtenTeller++;
+
+            winnerBepalen();
+        }
+
+        private void winnerBepalen()
+        {
+            bool winnaar = false;
+
+            //Horizonatale check
+            if ((A1.Content == A2.Content) && (A2.Content == A3.Content) && (!A1.IsEnabled))
+                winnaar = true;
+            else if ((B1.Content == B2.Content) && (B2.Content == B3.Content) && (!B1.IsEnabled))
+                winnaar = true;
+            else if ((C1.Content == C2.Content) && (C2.Content == C3.Content) && (!C1.IsEnabled))
+                winnaar = true;
+
+            //Verticale check
+            if ((A1.Content == B1.Content) && (B1.Content == C1.Content) && (!A1.IsEnabled))
+                winnaar = true;
+            else if ((A2.Content == B2.Content) && (B2.Content == C2.Content) && (!A2.IsEnabled))
+                winnaar = true;
+            else if ((A3.Content == B3.Content) && (B3.Content == C3.Content) && (!A3.IsEnabled))
+                winnaar = true;
+
+            //Diagonale check
+            
+            else if ((A1.Content == B2.Content) && (B2.Content == C3.Content) && (!A1.IsEnabled))
+                winnaar = true;
+            else if ((A3.Content == B2.Content) && (B2.Content == C1.Content) && (!C1.IsEnabled))
+                winnaar = true;
+
+            if (winnaar)
+            {
+                disableButtons();
+                String speler = "";
+                if (beurt)
+                    speler = "O";
+                else
+                    speler = "X";
+
+                MessageBox.Show(speler + " is gewonnen", "GG!");
+                
+            }
+            else
+            {
+                if (beurtenTeller == 9)
+                    MessageBox.Show("Gelijkspel!", "GG");
+            }
+
+        }//end
+
+        //methode buttons uitschakelen na er winnaar is
+        private void disableButtons()
+        {
+            GridMain.IsEnabled = false;
+        }
+    }
+}
